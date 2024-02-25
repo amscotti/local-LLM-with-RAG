93424e6e89b637e59a635a983274003dd135c4b5
Anthony Scotti <anthony.m.scotti@gmail.com>
2024-02-25 13:28:26 -0500

    Using Ollama for embeddings

--- app.py
+++ app.py
@@ -1,30 +1,21 @@
 from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
 from langchain_community.llms import Ollama
 from langchain_community.embeddings import OllamaEmbeddings
-from JinaAIEmbeddings import JinaAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-
-llm = Ollama(model="gemma", callbacks=[StreamingStdOutCallbackHandler()])
-
-# Define the question to be answered
-question = "What is the memory stream, and how does it help the agents?"
-
-directoryLoader = DirectoryLoader(
-    "PDFs",
-    glob="**/*.pdf",
-    loader_cls=PyPDFLoader,
-    show_progress=True,
-    use_multithreading=True,
-)
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+from models import check_if_model_is_available
+from document_loader import load_documents
+import argparse
+import sys
 
 
-prompt_template = """
+TEXT_SPLITTER = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+
+
+PROMPT_TEMPLATE = """
 ### Instruction:
 You're helpful assistant, who answers questions based upon provided research in a distinct and clear way.
 
@@ -35,20 +26,53 @@ You're helpful assistant, who answers questions based upon provided research in 
 {question}
 """
 
+
 PROMPT = PromptTemplate(
-    template=prompt_template, input_variables=["context", "question"]
+    template=PROMPT_TEMPLATE, input_variables=["context", "question"]
 )
 
 
-def main():
-    print("Loading documents into Chroma")
+def load_documents_into_database(model_name: str, documents_path: str) -> Chroma:
+    """
+    Loads documents from the specified directory into the Chroma database
+    after splitting the text into chunks.
 
-    # Initialize the directory loader
-    raw_documents = directoryLoader.load()
-    documents = text_splitter.split_documents(raw_documents)
+    Returns:
+        Chroma: The Chroma database with loaded documents.
+    """
 
-    # Load the embeddings into Chroma
-    db = Chroma.from_documents(documents, JinaAIEmbeddings())
+    print("Loading documents")
+    raw_documents = load_documents(documents_path)
+    documents = TEXT_SPLITTER.split_documents(raw_documents)
+
+    print("Creating embeddings and loading documents into Chroma")
+    db = Chroma.from_documents(
+        documents,
+        OllamaEmbeddings(model=model_name),
+    )
+    return db
+
+
+def main(llm_model_name: str, embedding_model_name: str, documents_path: str) -> None:
+    # Check to see if the models available, if not attempt to pull them
+    try:
+        check_if_model_is_available(llm_model_name)
+        check_if_model_is_available(embedding_model_name)
+    except Exception as e:
+        print(e)
+        sys.exit()
+
+    # Creating database form documents
+    try:
+        db = load_documents_into_database(embedding_model_name, documents_path)
+    except FileNotFoundError as e:
+        print(e)
+        sys.exit()
+
+    llm = Ollama(
+        model=llm_model_name,
+        callbacks=[StreamingStdOutCallbackHandler()],
+    )
 
     qa_chain = RetrievalQA.from_chain_type(
         llm,
@@ -56,9 +80,43 @@ def main():
         chain_type_kwargs={"prompt": PROMPT},
     )
 
-    print(f"\nAnswering question: {question}\n")
-    qa_chain.invoke({"query": question})
+    while True:
+        try:
+            user_input = input(
+                "\n\nPlease enter your question (or type 'exit' to end): "
+            )
+            if user_input.lower() == "exit":
+                break
+
+            docs = db.similarity_search(user_input)
+            qa_chain.invoke({"query": user_input})
+        except KeyboardInterrupt:
+            break
+
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Run local LLM with RAG with Ollama.")
+    parser.add_argument(
+        "-m",
+        "--model",
+        default="mistral",
+        help="The name of the LLM model to use.",
+    )
+    parser.add_argument(
+        "-e",
+        "--embedding_model",
+        default="nomic-embed-text",
+        help="The name of the embedding model to use.",
+    )
+    parser.add_argument(
+        "-p",
+        "--path",
+        default="Research",
+        help="The path to the directory containing documents to load.",
+    )
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-    main()
+    args = parse_arguments()
+    main(args.model, args.embedding_model, args.path)
---
+++ document_loader.py
@@ -0,0 +1,52 @@
+from langchain_community.document_loaders import (
+    DirectoryLoader,
+    PyPDFLoader,
+    TextLoader,
+)
+import os
+from typing import List
+from langchain_core.documents import Document
+
+
+def load_documents(path: str) -> List[Document]:
+    """
+    Loads documents from the specified directory path.
+
+    This function supports loading of PDF, Markdown, and HTML documents by utilizing
+    different loaders for each file type. It checks if the provided path exists and
+    raises a FileNotFoundError if it does not. It then iterates over the supported
+    file types and uses the corresponding loader to load the documents into a list.
+
+    Args:
+        path (str): The path to the directory containing documents to load.
+
+    Returns:
+        List[Document]: A list of loaded documents.
+
+    Raises:
+        FileNotFoundError: If the specified path does not exist.
+    """
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"The specified path does not exist: {path}")
+
+    loaders = {
+        ".pdf": DirectoryLoader(
+            path,
+            glob="**/*.pdf",
+            loader_cls=PyPDFLoader,
+            show_progress=True,
+            use_multithreading=True,
+        ),
+        ".md": DirectoryLoader(
+            path,
+            glob="**/*.md",
+            loader_cls=TextLoader,
+            show_progress=True,
+        ),
+    }
+
+    docs = []
+    for file_type, loader in loaders.items():
+        print(f"Loading {file_type} files")
+        docs.extend(loader.load())
+    return docs
