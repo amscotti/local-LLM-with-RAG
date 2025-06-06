42c4e1f4cbfb60fc4ea3f2a2b7ef22b26c26deb5
Neverov.p <pasha170813@gmail.com>
2025-06-06 10:26:19 +0300

    Для ДБ

---
+++ models_db.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from .database import Base
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, index=True)
+    login = Column(String(255), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    role_id = Column(Integer)
+    department_id = Column(Integer)
+    access_id = Column(Integer)
