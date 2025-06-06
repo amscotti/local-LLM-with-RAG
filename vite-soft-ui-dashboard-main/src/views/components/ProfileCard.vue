a78eae0b13dc8a441634eac1d3cf71f83609b2b2
Neverov.p <pasha170813@gmail.com>
2025-06-06 14:17:51 +0300

    играюсь с интерфейсом профиля

--- vite-soft-ui-dashboard-main/src/views/components/ProfileCard.vue
+++ vite-soft-ui-dashboard-main/src/views/components/ProfileCard.vue
@@ -3,27 +3,12 @@
     <div class="p-3 pb-0 card-header">
       <div class="row">
         <div class="col-md-8 d-flex align-items-center">
-          <h6 class="mb-0">Profile Information</h6>
-        </div>
-        <div class="col-md-4 text-end">
-          <a href="javascript:;">
-            <i
-              class="text-sm fas fa-user-edit text-secondary"
-              data-bs-toggle="tooltip"
-              data-bs-placement="top"
-              title="Edit Profile"
-            ></i>
-          </a>
+          <h6 class="mb-0">Информация о пользователе</h6>
         </div>
       </div>
     </div>
     <div class="p-3 card-body">
-      <p class="text-sm">
-        Hi, I’m Alec Thompson, Decisions: If you can’t decide, the answer is no.
-        If two equally difficult paths, choose the one more painful in the short
-        term (pain avoidance is creating an illusion of equality).
-      </p>
-      <hr class="my-4 horizontal gray-light" />
+      <hr class="my-1 horizontal gray-light" />
       <ul class="list-group">
         <li class="pt-0 text-sm border-0 list-group-item ps-0">
           <strong class="text-dark">Full Name:</strong> &nbsp; Alec M. Thompson
@@ -38,7 +23,7 @@
           <strong class="text-dark">Location:</strong> &nbsp; USA
         </li>
         <li class="pb-0 border-0 list-group-item ps-0">
-          <strong class="text-sm text-dark">Social:</strong> &nbsp;
+          <strong class="text-sm text-dark">Связь:</strong> &nbsp;
           <a
             class="py-0 mb-0 btn btn-facebook btn-simple ps-1 pe-2"
             href="javascript:;"
