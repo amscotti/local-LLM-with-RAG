0b5a6571b71cb9f1391db4c1bd74b2a67bfcd7f2
Neverov.p <pasha170813@gmail.com>
2025-06-06 08:20:01 +0300

    откат

--- backend/app/services/document_service.py
+++
@@ -1,6 +0,0 @@
-from fastapi import UploadFile
-from app.db.database import get_db
-
-def save_uploaded_file(file: UploadFile, db) -> dict:
-    # Логика для сохранения загруженного файла
-    return {"message": f"Файл '{file.filename}' успешно загружен."}
