adec1f282f0d4566945abf7a6d0f91496ec236f1
Neverov.p <pasha170813@gmail.com>
2025-06-09 11:14:12 +0300

    убраны лишние элементы

--- vite-soft-ui-dashboard-main/src/views/Dashboard.vue
+++ vite-soft-ui-dashboard-main/src/views/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="py-4 container-fluid">
-    <div class="row">
-      <div v-for="card in cards" :key="card.id" class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+    <div class="row flex-column">
+      <div v-for="card in cards" :key="card.id" class="col-12 mb-4">
         <card
           :title="card.title"
           :value="card.description"
@@ -11,111 +11,6 @@
           direction-reverse
           :userId="Number(userId)"
         ></card>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-lg-7 mb-lg-0 mb-4">
-        <div class="card">
-          <div class="card-body p-3">
-            <div class="row">
-              <div class="col-lg-6">
-                <div class="d-flex flex-column h-100">
-                  <p class="mb-1 pt-2 text-bold">Built by developers</p>
-                  <h5 class="font-weight-bolder">Vite Soft UI Dashboard</h5>
-                  <p class="mb-5">
-                    From colors, cards, typography to complex elements, you will
-                    find the full documentation.
-                  </p>
-                  <a
-                    class="text-body text-sm font-weight-bold mb-0 icon-move-right mt-auto"
-                    href="javascript:;"
-                  >
-                    Read More
-                    <i
-                      class="fas fa-arrow-right text-sm ms-1"
-                      aria-hidden="true"
-                    ></i>
-                  </a>
-                </div>
-              </div>
-              <div class="col-lg-5 ms-auto text-center mt-5 mt-lg-0">
-                <div class="bg-gradient-success border-radius-lg h-100">
-                  <img
-                    src="../assets/img/shapes/waves-white.svg"
-                    class="position-absolute h-100 w-50 top-0 d-lg-block d-none"
-                    alt="waves"
-                  />
-                  <div
-                    class="position-relative d-flex align-items-center justify-content-center h-100"
-                  >
-                    <img
-                      class="w-100 position-relative z-index-2 pt-4"
-                      src="../assets/img/illustrations/rocket-white.png"
-                      alt="rocket"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-5">
-        <div class="card h-100 p-3">
-          <div
-            class="overflow-hidden position-relative border-radius-lg bg-cover h-100"
-            style="
-              background-image: url('https://demos.creative-tim.com/soft-ui-dashboard/assets/img/ivancik.jpg');
-            "
-          >
-            <span class="mask bg-gradient-dark"></span>
-            <div
-              class="card-body position-relative z-index-1 d-flex flex-column h-100 p-3"
-            >
-              <h5 class="text-white font-weight-bolder mb-4 pt-2">
-                Work with the rockets
-              </h5>
-              <p class="text-white">
-                Wealth creation is an evolutionarily recent positive-sum game.
-                It is all about who take the opportunity first.
-              </p>
-              <a
-                class="text-white text-sm font-weight-bold mb-0 icon-move-right mt-auto"
-                href="javascript:;"
-              >
-                Read More
-                <i
-                  class="fas fa-arrow-right text-sm ms-1"
-                  aria-hidden="true"
-                ></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="mt-4 row">
-      <div class="mb-4 col-lg-5 mb-lg-0">
-        <div class="card z-index-2">
-          <div class="p-3 card-body">
-            <!-- chart -->
-            <active-users-chart />
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-7">
-        <!-- line chart -->
-        <div class="card z-index-2">
-          <gradient-line-chart />
-        </div>
-      </div>
-    </div>
-    <div class="row my-4">
-      <div class="col-lg-8 col-md-6 mb-md-0 mb-4">
-        <projects-card />
-      </div>
-      <div class="col-lg-4 col-md-6">
-        <Orders-card />
       </div>
     </div>
   </div>
@@ -154,7 +49,6 @@ export default {
       this.$router.push("/sign-in");
       return;
     }
-    console.log("User ID:", this.userId);
     await this.fetchCards(Number(this.userId)); // Загрузка карточек для текущего пользователя
   },
   methods: {
