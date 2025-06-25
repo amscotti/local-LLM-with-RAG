fe9e823aea26279151644324c8bb5fa3acb69a52
Neverov.p <pasha170813@gmail.com>
2025-06-25 11:58:27 +0300

    убрано лишнее

--- vite-soft-ui-dashboard-main/src/examples/Footer.vue
+++ vite-soft-ui-dashboard-main/src/examples/Footer.vue
@@ -3,21 +3,7 @@
     <div class="container-fluid">
       <div class="row align-items-center justify-content-lg-between">
         <div class="mb-4 col-lg-6 mb-lg-0">
-          <div
-            class="text-sm text-center copyright text-muted"
-            :class="$store.state.isRTL ? 'text-lg-end' : 'text-lg-start'"
-          >
-            ©
-            {{ new Date().getFullYear() }}, made with
-            <i class="fa fa-heart"></i> by
-            <a
-              href="https://www.creative-tim.com"
-              class="font-weight-bold"
-              target="_blank"
-              >Creative Tim</a
-            >
-            for a better web.
-          </div>
+ 
         </div>
         <div class="col-lg-6">
           <ul
